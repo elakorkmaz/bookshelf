f714c9b554bb7b9af4db02462a0d187de234c1ae
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-10-19 12:46:36 +0200

    added empty book pages

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ app.get('/', function(request, response) {
  // response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/books/', function(request, response) {
+ console.log('Requesting home page...');
+ response.send(pug.renderFile(__dirname + '/index.pug', {}));
+ // response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/books/harrypotter', function(request, response) {
+ console.log('Requesting home page...');
+ response.send(pug.renderFile(__dirname + '/books/harrypotter.pug', {}));
+ // response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/books/borntorun', function(request, response) {
+ console.log('Requesting home page...');
+ response.send(pug.renderFile(__dirname + '/books/borntorun.pug', {}));
+ // response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/books/clovermoon', function(request, response) {
+ console.log('Requesting home page...');
+ response.send(pug.renderFile(__dirname + '/books/clovermoon.pug', {}));
+ // response.sendFile(__dirname + '/index.html');
+});
+
 app.listen(3000, function() {
  console.log('Web server started on port 3000');
 });
